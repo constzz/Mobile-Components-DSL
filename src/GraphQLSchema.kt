f9c37ba502343cc9adb6250130136788dd7892c8
Konstantin Bezzemelnyi <konstantin.bezzemelnyi@gmail.com>
2022-09-12 21:38:20 +0300

    Update Schema: add GetTasksAssigned query to Retrieve all the tasks that are assigned on the user

--- src/GraphQLSchema.kt
+++ src/GraphQLSchema.kt
@@ -1,7 +1,14 @@
 package konstantin.bezzemelnyi
 
 import com.apurebase.kgraphql.schema.dsl.SchemaBuilder
+import konstantin.bezzemelnyi.mock.mockTasks
 
 fun SchemaBuilder.schemaValue() {
 
+    query("GetTasksAssigned") {
+        description = "Retrieve all the tasks that are assigned on the user."
+        resolver { ->
+            mockTasks
+        }
+    }
 }
